ea523e322f962542e4ba2004fbcedd85e3310b4b
Ananya9876 <ananyamnaik6@gmail.com>
2025-05-12 15:48:54 +0530

    console operator

--- JavaScript/first.js
+++ JavaScript/first.js
@@ -1,2 +1,36 @@
-console.log("Hiii javascript");
-console.log("Welcome to the chrome browser");
+
+
+// let fullName="Ananya Naik"
+// console.log(fullName);
+
+// console.log("Hiii javascript");
+// console.log("Welcome to the chrome browser");
+// const product={
+//     title: "Ball pen",
+//     rating: 4,
+//     offer: 5,
+//     price: 270
+// };
+
+// console.log(product);
+
+//Arithematic operator
+
+let a=5;
+let b=2;
+
+console.log("a + b = ",a+b);
+console.log("a - b = ",a-b);
+console.log("a * b = ",a*b);
+console.log("a / b = ",a/b);
+console.log("a ** b = ",a**b);
+console.log("a % b = ",a%b);
+
+
+// Unary operators
+a--;
+console.log(a);
+
+//comparision 
+
+

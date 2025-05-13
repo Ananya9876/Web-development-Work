cd96f5c4645923ca6c6898eab07c4b05772b1492
Ananya9876 <ananyamnaik6@gmail.com>
2025-05-13 18:19:25 +0530

    Loops and strings

--- JavaScript/first.js
+++ JavaScript/first.js
@@ -16,21 +16,105 @@
 
 //Arithematic operator
 
-let a=5;
-let b=2;
+// let a=5;
+// let b=2;
 
-console.log("a + b = ",a+b);
-console.log("a - b = ",a-b);
-console.log("a * b = ",a*b);
-console.log("a / b = ",a/b);
-console.log("a ** b = ",a**b);
-console.log("a % b = ",a%b);
+// console.log("a + b = ",a+b);
+// console.log("a - b = ",a-b);
+// console.log("a * b = ",a*b);
+// console.log("a / b = ",a/b);
+// console.log("a ** b = ",a**b);
+// console.log("a % b = ",a%b);
 
 
-// Unary operators
-a--;
-console.log(a);
+// // Unary operators
+// a--;
+// console.log(a);
 
 //comparision 
+
+
+//ternary
+
+
+// let age=21;
+
+// console.log(age>18?"adult":"not adult");
+
+
+// let num=prompt("Enter a number: ");
+
+// if(num%5==0){
+//     console.log("Its a multiple of 5");
+// } else {
+//     console.log("Its not a multiple of 5");
+// }
+
+
+
+
+
+
+
+
+
+
+// let score=69;
+
+// if(score>=80 && score<=100){
+//     console.log("A");
+// } else if(score>=70 && score<=79){
+//     console.log("B");
+// } else if(score>=60 && score<=69){
+//     console.log("C");
+// } else if(score>=50 && score<=59){
+//     console.log("D");
+// } else{
+//     console.log("F");
+// }
+
+
+// let name="Ananya";
+
+// for(let i of name){
+//     console.log("i= ", i);
+// }
+
+
+
+
+
+// let num=25;
+
+// let result=prompt("Enter a number");
+
+// while(result!=num){
+//     result=prompt("You entered wrong number......Guess again");
+// } 
+// console.log("The number you guessed is correct");
+
+
+
+
+//Template lietrals
+
+// let obj={
+//     name:"Paper",
+//     price:10
+// };
+
+// let op=`The cost of ${obj.name} is ${obj.price} rupees`;
+// console.log(op);
+
+
+
+//Strings
+
+// let name="Ananya Naik";
+
+// console.log(name);
+
+
+
 
 
